2bc856cf8ef4af9a628da570fc5f6781a04f869e
Sanal-T <sanalt2024@gmail.com>
2026-08-28 13:11:13 +0530

    History Option Deleted

--- src/app/history/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HistoryList } from '@/components/history-list';
-
-export default function HistoryPage() {
-    return (
-        <main className="flex-1 w-full max-w-7xl mx-auto px-4 py-8 overflow-y-auto">
-            <HistoryList />
-        </main>
-    );
-}
